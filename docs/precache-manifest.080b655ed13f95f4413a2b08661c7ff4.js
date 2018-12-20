71e7cd119a0292de42873528cd7de6cfab471452
Nguyen Hong Linh <linhnh@IBL022.localdomain>
2018-12-20 19:13:51 +0700

    Add the /docs (build by npm) for publishing)

---
+++ docs/precache-manifest.080b655ed13f95f4413a2b08661c7ff4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "beafe335ef80e3ce010c",
+    "url": "/react-tutorial/static/js/main.beafe335.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/react-tutorial/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "beafe335ef80e3ce010c",
+    "url": "/react-tutorial/static/css/main.88d50b00.chunk.css"
+  },
+  {
+    "revision": "603f8fb42a9ed7b9fa655bf9d7cf5338",
+    "url": "/react-tutorial/index.html"
+  }
+];
